425192bf30643f7ce3da5b6509135f19753f452f
MarcusErick <marcushp1@hotmail.com>
2024-02-15 16:34:49 -0600

    http client gifs service

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
 
   private _tagsHistory: string[] = [];
+  private apiKey: string = 'uGxCzOPAkDIEEyVz2qzPABzt923km4pz';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   get tagsHistory(){
     return [...this._tagsHistory];
@@ -27,6 +30,18 @@ export class GifsService {
   searchTag(tag: string):void {
     if (tag.length === 0) return;
     this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', tag)
+
+    this.http.get(`${this.serviceUrl}/search?`, { params })
+      .subscribe( resp => {
+
+        console.log(resp);
+
+      })
 
   }
 }
